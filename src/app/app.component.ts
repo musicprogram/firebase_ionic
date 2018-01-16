1e59cf4c37745f8e3a417f18027bb17169a95ad3
musicprogram <juan-o-henao@hotmail.es>
2018-01-16 09:01:48 -0500

    funcionando vista sign up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Crear cuenta', component: SignUpPage }
     ];
 
   }
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import * as firebase from 'firebase/app';
+
+import {firebaseConfig} from '../enviroments/firebase_config';
+//var config 
+firebase.initializeApp(firebaseConfig);
+
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
